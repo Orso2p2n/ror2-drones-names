59b863227874a49a601b979061c42e054f2c5c36
Orsopidou <Orsoph@gmail.com>
2022-12-07 19:43:42 +0100

    Hook on CharacterMaster.SpawnBody

--- DronesNames/DronesNames.cs
+++ DronesNames/DronesNames.cs
@@ -29,17 +29,76 @@ namespace DronesNames
 
             AddTokens();
 
-            On.RoR2.CharacterBody.Start += CharacterBody_Start;
+            On.RoR2.CharacterMaster.SpawnBody += CharacterMaster_SpawnBody;
+            On.RoR2.CharacterMaster.OnBodyDeath += CharacterMaster_OnBodyDeath;
+
+            On.RoR2.CharacterMaster.OnSerialize += CharacterMaster_OnSerialize;
+            On.RoR2.CharacterMaster.OnDeserialize += CharacterMaster_OnDeserialize;
         }
 
-        private void CharacterBody_Start(On.RoR2.CharacterBody.orig_Start orig, RoR2.CharacterBody self)
+        // HOOKS
+        private CharacterBody CharacterMaster_SpawnBody(On.RoR2.CharacterMaster.orig_SpawnBody orig, CharacterMaster self, Vector3 position, Quaternion rotation)
         {
-            orig(self);
+            var body = orig(self, position, rotation);
 
-            OnCharacterBodySpawned(self);
+            if (body != null)
+            {
+                OnCharacterBodySpawned(self, body);
+            }
+
+            return body;
         }
 
-        private void OnCharacterBodySpawned(CharacterBody characterBody)
+        private void CharacterMaster_OnBodyDeath(On.RoR2.CharacterMaster.orig_OnBodyDeath orig, CharacterMaster self, CharacterBody body)
+        {
+            orig(self, body);
+        }
+
+        private bool CharacterMaster_OnSerialize(On.RoR2.CharacterMaster.orig_OnSerialize orig, CharacterMaster self, UnityEngine.Networking.NetworkWriter writer, bool initialState) {
+            var b = orig(self, writer, initialState);
+
+            if (self == null) {
+                return b;
+            }
+
+            var characterBody = self.GetBody();
+
+            if (characterBody == null) {
+                return b;
+            }
+
+            var bodyName = BodyCatalog.GetBodyName(BodyCatalog.FindBodyIndex(characterBody));
+            var displayName = characterBody.GetDisplayName();
+            var bodyInstanceId = self.networkIdentity.netId.Value;
+            Log.LogDebug("SERIALIZE " + bodyName + " " + displayName + " " + bodyInstanceId);
+
+            return b;
+        }
+
+        private void CharacterMaster_OnDeserialize(On.RoR2.CharacterMaster.orig_OnDeserialize orig, CharacterMaster self, UnityEngine.Networking.NetworkReader reader, bool initialState) {
+            orig(self, reader, initialState);
+
+            
+
+            if (self == null) {
+                return;
+            }
+
+            var characterBody = self.GetBody();
+
+            if (characterBody == null) {
+                return;
+            }
+
+            var bodyName = BodyCatalog.GetBodyName(BodyCatalog.FindBodyIndex(characterBody));
+            var displayName = characterBody.GetDisplayName();
+            var bodyInstanceId = self.networkIdentity.netId.Value;
+            Log.LogDebug("DESERIALIZE " + bodyName + " " + displayName + " " + bodyInstanceId);
+        }
+
+
+        // FUNCTIONS
+        private void OnCharacterBodySpawned(CharacterMaster characterMaster, CharacterBody characterBody)
         {
             // if (characterBody.master.teamIndex != RoR2.TeamIndex.Player) {
             //     return;
@@ -54,7 +113,15 @@ namespace DronesNames
 
             characterBody.baseNameToken = newName;
 
-            Log.LogDebug(BodyCatalog.GetBodyName(BodyCatalog.FindBodyIndex(characterBody)) + " " + characterBody.GetDisplayName());
+            // Log.LogDebug( + " " + );
+            #pragma warning disable Publicizer001
+
+            var bodyName = BodyCatalog.GetBodyName(BodyCatalog.FindBodyIndex(characterBody));
+            var displayName = characterBody.GetDisplayName();
+            var bodyInstanceId = characterMaster.networkIdentity.netId.Value;
+            Log.LogDebug("SPAWN " + bodyName + " " + displayName + " " + bodyInstanceId);
+
+            #pragma warning restore Publicizer001
         }
 
         private void AddTokens()
