970212adbf1cba46613b0aa8c895e0f2b266e680
Orsopidou <Orsoph@gmail.com>
2022-12-12 00:38:49 +0100

    NamesByBodyNames is now a struct, added possibility to select which list for each entry, removed placeholders names, added some names

--- DronesNames/NamesList.cs
+++ DronesNames/NamesList.cs
@@ -3,42 +3,56 @@ using System.Linq;
 
 namespace DronesNames
 {
+    public struct NamesByBodyNames
+    {
+        public string bodyName { get; }
+        public string[] additionalIndexes { get; }
+
+        public Dictionary<string, string> names;
+
+        public NamesByBodyNames(string bodyName, string[] additionalIndexes)
+        {
+            this.bodyName = bodyName;
+            this.additionalIndexes = additionalIndexes;
+            
+            names = new Dictionary<string, string>();
+        }
+    }
+
     public class NamesList
     {
         public static Xoroshiro128Plus rng = new Xoroshiro128Plus(0);
 
-        public static Dictionary<string, Dictionary<string, string>> namesByBodyNames = new Dictionary<string, Dictionary<string, string>>()
+        public static List<NamesByBodyNames> namesByBodyNames = new List<NamesByBodyNames>()
         {
-            { "EmergencyDroneBody",       new Dictionary<string, string>() },       // (Emergency drone)
-            { "Turret1Body",              new Dictionary<string, string>() },       // (Gunner turret)
-            { "Drone1Body",               new Dictionary<string, string>() },       // (Gunner drone)
-            { "Drone2Body",               new Dictionary<string, string>() },       // (Healing drone)
-            { "FlameDroneBody",           new Dictionary<string, string>() },       // (Incinerator drone)
-            { "MissileDroneBody",         new Dictionary<string, string>() },       // (Missile drone)
-            { "EquipmentDroneBody",       new Dictionary<string, string>() },       // (Equipment drone)
-            { "MegaDroneBody",            new Dictionary<string, string>() },       // (TC-280)
-            { "DroneCommanderBody",       new Dictionary<string, string>() },       // (Col. Droneman)
-            { "BackupDroneBody",          new Dictionary<string, string>() },       // (Strike drone)
-            { "BeetleGuardAllyBody",      new Dictionary<string, string>() },       // (Beetle guard)
-            { "SquidTurretBody",          new Dictionary<string, string>() },       // (Squid polyp)
-            { "RoboBallRedBuddyBody",     new Dictionary<string, string>() },       // (Empathy core 1)
-            { "RoboBallGreenBuddyBody",   new Dictionary<string, string>() },       // (Empathy core 2)
-            { "MinorConstructOnKillBody", new Dictionary<string, string>() }        // (Defense nucleus)
+            { new NamesByBodyNames("EmergencyDroneBody",          new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("Turret1Body",                 new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("Drone1Body",                  new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("Drone2Body",                  new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("FlameDroneBody",              new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("MissileDroneBody",            new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("EquipmentDroneBody",          new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("MegaDroneBody",               new string[0] {}                      ) },
+            { new NamesByBodyNames("DroneCommanderBody",          new string[0] {}                      ) },
+            { new NamesByBodyNames("BackupDroneBody",             new string[1] { "DefaultDrones" }     ) },
+            { new NamesByBodyNames("BeetleGuardAllyBody",         new string[0] {}                      ) },
+            { new NamesByBodyNames("SquidTurretBody",             new string[0] {}                      ) },
+            { new NamesByBodyNames("RoboBallRedBuddyBody",        new string[0] {}                      ) },
+            { new NamesByBodyNames("RoboBallGreenBuddyBody",      new string[0] {}                      ) },
+            { new NamesByBodyNames("MinorConstructOnKillBody",    new string[0] {}                      ) },
         };
 
         public static void BuildNamesByBodyName()
         {
-            foreach (var bodyName in namesByBodyNames.Keys)
+            foreach (var namesByBodyName in namesByBodyNames)
             {
                 foreach (var possibleIndexes in names.Keys)
                 {
                     var addNames = false;
                     
                     foreach (var possibleIndex in possibleIndexes)
-                    {
-                        var possibleBodyIndex = possibleIndex;
-                        
-                        if (possibleIndex == "Default" || possibleBodyIndex == bodyName)
+                    {                        
+                        if (namesByBodyName.bodyName == possibleIndex || namesByBodyName.additionalIndexes.Contains(possibleIndex))
                         {
                             addNames = true;
                             break;
@@ -50,7 +64,7 @@ namespace DronesNames
                     {
                         foreach (var name in names[possibleIndexes])
                         {
-                            namesByBodyNames[bodyName].Add(name.Key, name.Value);
+                            namesByBodyName.names.Add(name.Key, name.Value);
                         }
                     }
                 }
@@ -79,13 +93,24 @@ namespace DronesNames
             var bodyName = RoR2.BodyCatalog.GetBodyName(bodyIndex);
 
             // If it's not in the list of names, skip
-            if (!namesByBodyNames.ContainsKey(bodyName))
+            var namesDictionary = new Dictionary<string, string>();
+            var foundDictionary = false;
+            foreach (var namesByBodyName in namesByBodyNames)
+            {
+                if (namesByBodyName.bodyName == bodyName)
+                {
+                    namesDictionary = namesByBodyName.names;
+                    foundDictionary = true;
+                    break;
+                }
+            }
+
+            if (!foundDictionary)
             {
                 return "";
             }
 
             // Get the names dictionary <token,name>. If it's empty, skip
-            var namesDictionary = namesByBodyNames[bodyName];
             if (namesDictionary.Count == 0)
             {
                 return "";
@@ -107,38 +132,8 @@ namespace DronesNames
 
         public static Dictionary<string[],Dictionary<string,string>> names = new Dictionary<string[], Dictionary<string, string>>
         {
-            { new string[1] { "Default" }, new Dictionary<string,string>
+            { new string[1] { "DefaultDrones" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_DEFAULT", "Default" },
-                    { "DRONESNAMES_CHRISTOPHER", "Christopher" },
-                    { "DRONESNAMES_JESSICA", "Jessica" },
-                    { "DRONESNAMES_MATTHEW", "Matthew" },
-                    { "DRONESNAMES_ASHLEY", "Ashley" },
-                    { "DRONESNAMES_JENNIFER", "Jennifer" },
-                    { "DRONESNAMES_JOSHUA", "Joshua" },
-                    { "DRONESNAMES_AMANDA", "Amanda" },
-                    { "DRONESNAMES_DANIEL", "Daniel" },
-                    { "DRONESNAMES_DAVID", "David" },
-                    { "DRONESNAMES_JAMES", "James" },
-                    { "DRONESNAMES_ROBERT", "Robert" },
-                    { "DRONESNAMES_JOHN", "John" },
-                    { "DRONESNAMES_JOSEPH", "Joseph" },
-                    { "DRONESNAMES_ANDREW", "Andrew" },
-                    { "DRONESNAMES_RYAN", "Ryan" },
-                    { "DRONESNAMES_BRANDON", "Brandon" },
-                    { "DRONESNAMES_JASON", "Jason" },
-                    { "DRONESNAMES_JUSTIN", "Justin" },
-                    { "DRONESNAMES_SARAH", "Sarah" },
-                    { "DRONESNAMES_WILLIAM", "William" },
-                    { "DRONESNAMES_JONATHAN", "Jonathan" },
-                    { "DRONESNAMES_STEPHANIE", "Stephanie" },
-                    { "DRONESNAMES_BRIAN", "Brian" },
-                    { "DRONESNAMES_NICOLE", "Nicole" },
-                    { "DRONESNAMES_NICHOLAS", "Nicholas" },
-                    { "DRONESNAMES_ANTHONY", "Anthony" },
-                    { "DRONESNAMES_HEATHER", "Heather" },
-                    { "DRONESNAMES_ERIC", "Eric" },
-                    { "DRONESNAMES_ELIZABETH", "Elizabeth" },
                     { "DRONESNAMES_JPEG", ".jpeg" },
                     { "DRONESNAMES_MICHAEL", "Michael" },
                     { "DRONESNAMES_MICROWAVE", "Microwave" },
@@ -159,92 +154,107 @@ namespace DronesNames
 
             { new string[1] { "EmergencyDroneBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_EMERGENCYDRONEBODY", "EmergencyDroneBody" },
+                    
                 }
             },
 
             { new string[1] { "Turret1Body" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_TURRET1BODY", "Turret1Body" },
-                    { "DRONESNAMES_MYBESTFRIEND", "My Best Friend" },
+                    { "DRONESNAMES_USELESS", "Useless" },
+                    { "DRONESNAMES_FLIGHTLESSDRONE", "Flightless Drone" },
                 }
             },
 
             { new string[1] { "Drone1Body" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_DRONE1BODY", "Drone1Body" },
+
                 }
             },
 
             { new string[1] { "Drone2Body" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_DRONE2BODY", "Drone2Body" },
+
                 }
             },
 
             { new string[1] { "FlameDroneBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_FLAMEDRONEBODY", "FlameDroneBody" },
+
                 }
             },
 
             { new string[1] { "MissileDroneBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_MISSILEDRONEBODY", "MissileDroneBody" },
+
                 }
             },
 
             { new string[1] { "EquipmentDroneBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_EQUIPMENTDRONEBODY", "EquipmentDroneBody" },
+
                 }
             },
 
             { new string[1] { "MegaDroneBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_MEGADRONEBODY", "MegaDroneBody" },
+                    { "DRONESNAMES_DESTRUCTION", "DESTRUCTION" },
+                    { "DRONESNAMES_APOCALYPSE", "APOCALYPSE" },
+                    { "DRONESNAMES_ARMAGEDDON", "ARMAGEDDON" },
+                    { "DRONESNAMES_BADINVESTMENT", "BAD INVESTMENT" },
                 }
             },
 
             { new string[1] { "DroneCommanderBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_DRONECOMMANDERBODY", "DroneCommanderBody" },
+                    { "DRONESNAMES_COLONELBEEPBOOP", "Colonel Beep Boop" },
                 }
             },
 
             { new string[1] { "BackupDroneBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_BACKUPDRONEBODY", "BackupDroneBody" },
+
                 }
             },
 
             { new string[1] { "BeetleGuardAllyBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_BEETLEGUARDALLYBODY", "BeetleGuardAllyBody" },
+                    { "DRONESNAMES_SIRDIESALOT", "Sir Dies-A-Lot" },
                 }
             },
 
             { new string[1] { "SquidTurretBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_SQUIDTURRETBODY", "SquidTurretBody" },
+                    { "DRONESNAMES_PEARL", "Pearl" },
+                    { "DRONESNAMES_MARINA", "Marina" },
+                    { "DRONESNAMES_FRYE", "Frye" },
+                    { "DRONESNAMES_SHIVER", "Shiver" },
+                    { "DRONESNAMES_AGENT3", "Agent 3" },
+                    { "DRONESNAMES_AGENT4", "Agent 4" },
+                    { "DRONESNAMES_CALLIE", "Callie" },
+                    { "DRONESNAMES_MARIE", "Marie" },
+                    { "DRONESNAMES_FREESCRAP", "Free Scrap" },
                 }
             },
 
             { new string[1] { "RoboBallRedBuddyBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_YESILOVEYOUTOO", "YES I LOVE YOU TOO" },
+                    { "DRONESNAMES_DOYOULOVETHEMTOO", "DO YOU LOVE THEM TOO" },
+                    { "DRONESNAMES_REDANDLONELY", "RED AND LONELY" },
+                    { "DRONESNAMES_COLD", "COLD" },
                 }
             },
 
             { new string[1] { "RoboBallGreenBuddyBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_DOYOULOVETHEMTOO", "DO YOU LOVE THEM TOO" },
+                    { "DRONESNAMES_YESILOVEYOUTOO", "YES I LOVE YOU TOO" },
+                    { "DRONESNAMES_GREENANDLONELY", "GREEN AND LONELY" },
+                    { "DRONESNAMES_BOLD", "BOLD" },
                 }
             },
 
             { new string[1] { "MinorConstructOnKillBody" }, new Dictionary<string,string>
                 {
-                    { "DRONESNAMES_MINORCONSTRUCTONKILLBODY", "MinorConstructOnKillBody" },
+                    { "DRONESNAMES_TRIANGLE", "Triangle" },
                 }
             },
         };
